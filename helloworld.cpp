8f193dffec0c9693de7c5cc29bed3013255a544c
yingchen123541 <54613135+yingchen123541@users.noreply.github.com>
2020-02-11 11:16:15 -0800

    Add files via upload

---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+using namespace std;
+
+int main(int argc, char** argv)
+{
+  string test = argv[1];
+  cout << "please enter a string" << endl;
+  cin >> test;
+
+  cout << "helloworld" << endl;
+}
